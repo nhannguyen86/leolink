6f5e0315e49b5cb91f2645934abd587b967f7c8d
phongtran2210 <thanhphong2210@gmail.com>
2016-09-14 11:16:03 +0700

    a

--- wp-content/themes/matata/header.php
+++ wp-content/themes/matata/header.php
@@ -110,10 +110,10 @@
 			endif; ?>
 		</div> .site-branding -->
 
-		<!--<nav id="site-navigation" class="main-navigation" role="navigation">
+		<nav id="site-navigation" class="main-navigation" role="navigation">
 			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><i class="fa fa-list-ul fa-2x"></i></button>
-			< ? php //wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
-		</nav> #site-navigation -->
+			<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
+		</nav> <!--#site-navigation -->
 		
 		<div id="logo">
 			<?php if ( get_header_image() ) : ?>
